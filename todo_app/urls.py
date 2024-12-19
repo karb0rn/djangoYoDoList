ef76fdd42c23fd32a65ac63a058e111fb2fbb147
shiva karthick <shiva.karthick07022004@gmail.com>
2024-12-19 15:12:35 +0530

    Updated the views and corrected all the errors

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,24 +1,12 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path("",views.ListListView.as_view(),name = "index"),
-    path("list/<int:list_id>/",views.ItemListView.as_view(),name = "list"),
-        path(
-        "list/<int:pk>/delete/", views.ListDelete.as_view(), name="list-delete"
-    ),
-    path(
-        "list/<int:list_id>/item/add/",
-        views.ItemCreate.as_view(),
-        name="item-add",
-    ),
-    path(
-        "list/<int:list_id>/item/<int:pk>/",
-        views.ItemUpdate.as_view(),
-        name="item-update",
-    ),
-        path(
-        "list/<int:list_id>/item/<int:pk>/delete/",
-        views.ItemDelete.as_view(),
-        name="item-delete",
-    ),
+    path('', views.ListListView.as_view(), name='index'),  # Home page for listing to-do lists
+    path('list/<int:list_id>/', views.ItemListView.as_view(), name='list'),  # List view for each to-do list
+    path('list/add/', views.ListCreate.as_view(), name='list-add'),  # Add a new to-do list
+    path('list/<int:list_id>/item/add/', views.ItemCreate.as_view(), name='item-add'),  # Add a new to-do item
+    path('list/<int:list_id>/item/<int:pk>/edit/', views.ItemUpdate.as_view(), name='item-edit'),  # Edit an item
+    path('list/<int:list_id>/delete/', views.ListDelete.as_view(), name='list-delete'),  # Delete a list
+    path('list/<int:list_id>/item/<int:pk>/delete/', views.ItemDelete.as_view(), name='item-delete'),  # Delete an item
 ]
